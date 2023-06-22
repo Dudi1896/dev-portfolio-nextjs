4e32dc3bd89b0b4a8805bfca7b5f2168c45e4c7c
Dudi1896 <denzelu181@gmail.com>
2023-06-22 16:29:31 -0400

    tailwinds configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,7 +9,20 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mont: ['var(--font-mont)', ...fontFamily.sans],
+      },
+      colors:{
+        colors: {
+          dark: "#1b1b1b",
+          light: "#f5f5f5",
+          primary: "#B63E96", // 240,86,199
+          primaryDark: "#58E6D9", // 80,230,217
+          indigoBlue: "#66a6f4",
+      }
+      }
+    },
   },
   plugins: [],
 }
