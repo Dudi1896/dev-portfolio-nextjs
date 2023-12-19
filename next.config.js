f7c4245324783260bef067c5f271b9dc98c7809c
Dudi1896 <denzelu181@gmail.com>
2023-12-19 16:01:59 -0500

    te

--- next.config.js
+++ next.config.js
@@ -3,11 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
 
   images: {
-    domains: [''],
+    domains: ['denzeludemba.com'],
+    domains: ['6581f9c18fe8155bc295a0ba--magnificent-bubblegum-c1842c.netlify.app'],
+    domains: ['denzeludemba.render.com'],
   },
+  rules: {
+    "@next/next/no-img-element": "off",
+   },
+  
 };
 
 module.exports ={
   ...nextConfig, // Include the Next.js configuration
-  output: 'standalone', // Your additional configuration
 };
